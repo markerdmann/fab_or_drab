c516a92896f5fa79b0073d5571cd00be6bf2b07a
Mark Sands <marksands07@gmail.com>
2010-05-29 23:21:44 -0700

    added camera shiz.

--- fabordrab/Classes/FabOrDrab.h
+++ fabordrab/Classes/FabOrDrab.h
@@ -8,13 +8,16 @@
 
 #import <UIKit/UIKit.h>
 
-@interface FabOrDrab : UIViewController
+@interface FabOrDrab : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 {
 	UIBarButtonItem *loginButton;
+	UIView *view;
 }
 
 @property (nonatomic, retain) UIBarButtonItem *loginButton;
+@property (nonatomic, retain) IBOutlet UIView *view;
 
 - (void) login:(id)sender;
+- (void) snap:(id)sender;
 
 @end
--- fabordrab/Classes/FabOrDrab.m
+++ fabordrab/Classes/FabOrDrab.m
@@ -7,20 +7,29 @@
 //
 
 #import "FabOrDrab.h"
+#import "AppHelpers.h"
+
 #import "TwitterLoginView.h"
+#import "Camera.h"
 
 @implementation FabOrDrab
 
 @synthesize loginButton;
+@synthesize view;
 
 - (id) init
-{
-	NSLog(@"Can you hear me now?!");
-	
+{	
 	if ( self = [super init] ) {
 		// success!
-		loginButton = [[UIBarButtonItem alloc] initWithTitle:@"SignIn" style:UIBarButtonItemStylePlain target:self action:@selector(login:)];
+		self.title = @"Fab or Drab";
+
+		loginButton = [[UIBarButtonItem alloc] initWithTitle:@"Sign in" style:UIBarButtonItemStylePlain target:self action:@selector(login:)];
 		self.navigationItem.rightBarButtonItem = loginButton;
+		
+		UIBarButtonItem *cameraBtn = [[UIBarButtonItem alloc] initWithTitle:@"Camera" style:UIBarButtonItemStylePlain target:self action:@selector(snap:)];
+		self.navigationItem.leftBarButtonItem  = cameraBtn;
+		
+		view = [[UIView alloc] init];
 	}
 
 	return self;
@@ -30,14 +39,55 @@
 {
 	TwitterLoginView *next = [[TwitterLoginView alloc] init];
 	[self.navigationController pushViewController:next animated:YES];
-	[next release];	
+	[next release];
+	
+}
+
+- (void) snap:(id)sender
+{	
+  UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+
+	if([UIImagePickerController isSourceTypeAvailable: UIImagePickerControllerSourceTypeCamera]) {
+		imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+	} 
+	else {
+		imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+	}
+
+	imagePicker.allowsEditing = NO;
+  
+	imagePicker.delegate = self;
+	imagePicker.allowsEditing = NO;
+	
+	[self.navigationController presentModalViewController:imagePicker animated:YES];	
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+	UIImage *image  = [info objectForKey:@"UIImagePickerControllerOriginalImage"];
+	
+	UIImageView *imageView2 = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, 480)];
+	imageView2.image = image;
+
+	NSLog(@"image: %@", image);
+
+	//UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+	[picker release];
+
+	[self.navigationController dismissModalViewControllerAnimated:YES];		
+}
+
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
+{	
+	if (error)
+		AlertWithErrorAndDelegate(error, nil);
 }
 
 - (void)dealloc
 {
-	[super dealloc];
 	[loginButton release];
+	[view release];
+	[super dealloc];
 }
-
 
 @end
